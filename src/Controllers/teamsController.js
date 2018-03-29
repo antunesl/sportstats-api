3d1a754607bed8c21767d2f8741004f281951e92
Luis Antunes <luis.antunes@wig.pt>
2018-03-29 13:19:39 +0100

    bugfix

--- src/Controllers/teamsController.js
+++ src/Controllers/teamsController.js
@@ -201,34 +201,29 @@ class TeamsController extends BaseController {
             limit: 10
         };
 
-        TeamInfo.find({ hasPreview: true, previewScrapDone: false }, options)
-            .project({
-                _id: 1,
-                permalink: 1,
-                nextGame: 1
-            })
-            .then(function (dbTeams) {
-                logger.info('dbResult: ' + JSON.stringify(dbTeams));
-
-                var result = [];
-                var ids = [];
-                dbTeams.forEach(team => {
-                    logger.info(' » Getting preview link for team "' + team.name + '": ' + team.nextGame.previewLink);
-                    ids.push(team.permalink);
-                    result.push({
-                        home: team.permalink,
-                        link: team.nextGame.previewLink
-                    });
-                });
-
-                logger.info('result:' + JSON.stringify(result));
-
-                return res.json(responseModel.successResponse(result));
-            })
-            .catch(function (err) {
+        TeamInfo.find({ hasPreview: true, previewScrapDone: false }, options, function (err, data) {
+            if (err) {
                 logger.error(err);
                 return res.status(500).json(responseModel.errorResponse(err));
+            }
+
+            logger.info('dbResult: ' + JSON.stringify(dbTeams));
+
+            var result = [];
+            var ids = [];
+            dbTeams.forEach(team => {
+                logger.info(' » Getting preview link for team "' + team.name + '": ' + team.nextGame.previewLink);
+                ids.push(team.permalink);
+                result.push({
+                    home: team.permalink,
+                    link: team.nextGame.previewLink
+                });
             });
+
+            logger.info('result:' + JSON.stringify(result));
+
+            return res.json(responseModel.successResponse(result));
+        });
     }
 
 
